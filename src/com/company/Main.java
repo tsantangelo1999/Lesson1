da79d8542411eae66b8f84e713fc3b51f9696c88
Thomas M Santangelo <ts159@pvhs-lab112-d19.pvsd.org>
2016-09-07 14:20:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+	    System.out.println("Hello world"); //output hello world
+        System.out.print("What is going on?");
+        //2 comments <- line comment
+        /* block comment */
+        System.out.println(8);
+        System.out.println(8.0);
+    }
+}
